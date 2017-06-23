646f90fa92cc138f5e31a4b3659c1ad1a5c98e65
mrlinds <mrlinds@gmail.com>
2017-06-23 15:03:51 -0700

    spelling and spacing

--- Utilities/LockOrientation.cs
+++ Utilities/LockOrientation.cs
@@ -1,23 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
 namespace InspectorTween{
-[AddComponentMenu("InspectorTween/Constraints/lockorientation",0)]
-[ExecuteInEditMode]
-
-public class LockOrientation : MonoBehaviour {
-	/*public bool lock_x;
-	public bool lock_y;
-	public bool lock_z;*/
-	public Vector3 worldRotation;
-	private Quaternion _worldRotation;
-	// Use this for initialization
-	void OnEnable () {
-		_worldRotation = Quaternion.Euler(worldRotation);
+	[AddComponentMenu("InspectorTween/Constraints/Lock Orientation",0)]
+	[ExecuteInEditMode]
+	public class LockOrientation : MonoBehaviour {
+		public Vector3 worldRotation;
+		private Quaternion _worldRotation;
+		void OnEnable () {
+			_worldRotation = Quaternion.Euler(worldRotation);
+		}
+		void LateUpdate () {
+			this.transform.rotation = _worldRotation;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		this.transform.rotation = _worldRotation;
-	}
-}
 }
