7201c79841b0f8bc58856fe01861aaf178fcc0d8
Galen Ryder <galen@radialgames.com>
2017-11-29 18:54:28 -0800

    Added a TweenAnchoredPosition for UI

---
+++ TweenAnchoredPosition.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace InspectorTween
+{
+    public class TweenAnchoredPosition : TweenBase
+    {
+        public Vector2 from;
+        public Vector2 to;
+        private RectTransform _rectTransform;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            _rectTransform = (RectTransform)transform;
+        }
+
+        protected override void LerpParameters(float lerp)
+        {
+            _rectTransform.anchoredPosition = Vector2.Lerp(from, to, lerp);
+        }
+    }
+}
