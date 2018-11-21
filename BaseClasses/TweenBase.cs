e836c67fd32aa83cad84681debf032854ae7dc87
Lindsay <mrlinds@gmail.com>
2018-11-20 17:11:28 -0800

    don't count persistent events before firing as this stops events added from script from firing if they're added

--- BaseClasses/TweenBase.cs
+++ BaseClasses/TweenBase.cs
@@ -1182,7 +1182,7 @@ namespace InspectorTween {
 
 			LerpParameters(lerpVal);
 			if(isOkToFireEvent(events.onCompleteCondition,reverse) ) {
-				if ( onLoopComplete!= null && onLoopComplete.GetPersistentEventCount() > 0 ) {
+				if ( onLoopComplete!= null  ) {
 					onLoopComplete.Invoke();
 				}
 			}
